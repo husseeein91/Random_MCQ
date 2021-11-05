3d4a4f45d4d9352b06a3e858ffa7ea9578883dd3
Mohamed Hussein <77590606+husseeein91@users.noreply.github.com>
2021-11-05 21:37:22 +0200

    Add files via upload

---
+++ client/src/actions/types.js
@@ -0,0 +1,6 @@
+export const GET_EXAM = "GET_EXAM";
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
+export const GET_QUESTION = "GET_QUESTION";
+export const GET_RESULT = "GET_RESULT";
+export const EXAM_ERROR = "EXAM_ERROR";
+export const SET_NAME = "SET_NAME";
